6fb670c086c1ac34d1c4e0ef93f1637585d9caf3
Mohd Ramzan Shareef <107868540+itisRamzan@users.noreply.github.com>
2024-07-14 12:09:51 +0530

    XOR

--- src/Bits.java
+++ src/Bits.java
@@ -1,5 +1,11 @@
 public class Bits {
     public static void main(String[] args) {
+        // 0 ^ 0 --> 0
+        // 1 ^ 1 --> 0
+        // 0 ^ 1 --> 1
+        // 1 ^ 0 --> 1
+        // same numbers XOR --> 0
+
         // To Access first bit of integer n --> n&1
         // n ^ n --> 0 --> n XOR n =0
         // To add 1 to last of zeroes n(000000) do `n<<1 | 1`

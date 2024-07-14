f4f1074d6502bf33103c0c74d39b9b029c028b33
Mohd Ramzan Shareef <107868540+itisRamzan@users.noreply.github.com>
2024-07-14 12:04:09 +0530

    Bit

--- src/Bits.java
+++ src/Bits.java
@@ -1,9 +1,9 @@
 public class Bits {
     public static void main(String[] args) {
         // To Access first bit of integer n --> n&1
-        // n & n --> 0
+        // n ^ n --> 0 --> n XOR n =0
         // To add 1 to last of zeroes n(000000) do `n<<1 | 1`
-        int[] arr = {1,2,3,4,5};
+        int[] arr = { 1, 2, 3, 4, 5 };
         for (int i : arr) {
             System.out.println(i);
         }

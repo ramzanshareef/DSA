e04dd93ec85c1a732179802e976da3b3e8dcacd4
Mohd Ramzan Shareef <107868540+itisRamzan@users.noreply.github.com>
2024-07-21 11:50:49 +0530

    ARRAY: Sort Colors

--- src/Arrays/Basics.java
+++ src/Arrays/Basics.java
@@ -1,7 +1,6 @@
 package Arrays;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 /**
@@ -81,6 +80,28 @@ public class Basics {
         }
         return new int[] { -1, -1 };
     }
+
+    public static void sortColors(int[] nums) {
+        int low = 0, mid = 0, high = nums.length - 1;
+        while (mid <= high) {
+            if (nums[mid] == 0) {
+                int temp = nums[low];
+                nums[low] = nums[mid];
+                nums[mid] = temp;
+                low++;
+                mid++;
+            } else if (nums[mid] == 1) {
+                mid++;
+            } else {
+                int temp = nums[mid];
+                nums[mid] = nums[high];
+                nums[high] = temp;
+                high--;
+            }
+        }
+    }
+
+    
 
 
 }

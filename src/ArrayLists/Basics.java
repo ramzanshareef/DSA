45c7eba8a85d7dc8c25bd540edb708af8eb25f68
Mohd Ramzan Shareef <107868540+itisRamzan@users.noreply.github.com>
2024-07-22 15:27:29 +0530

    ARRAYLISTS: Sorting and reversing Array Lists

--- src/ArrayLists/Basics.java
+++ src/ArrayLists/Basics.java
@@ -1,5 +1,7 @@
 package ArrayLists;
+
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class Basics {
     public static void main(String[] args) {
@@ -19,14 +21,34 @@ public class Basics {
     }
 
     public static void reverseArrayList(ArrayList<Integer> list) {
-        int i = 0, j = list.size() - 1;
-        while (i<j) {
-            Integer temp = Integer.valueOf(list.get(i));
-            list.set(i, list.get(j));
-            list.set(j, temp);
-            i++;
-            j--;
-        }
+        // int i = 0, j = list.size() - 1;
+        // while (i<j) {
+        // Integer temp = Integer.valueOf(list.get(i));
+        // list.set(i, list.get(j));
+        // list.set(j, temp);
+        // i++;
+        // j--;
+        // }
+        Collections.reverse(list);
         System.out.println(list);
     }
+
+    // ArrayList<Integer> list = new ArrayList<>();
+    // list.add(0);
+    // list.add(10);
+    // list.add(3);
+    // list.add(5);
+    // list.add(22);
+    // list.add(10);
+    // System.out.println(list);
+    // Collections.sort(list, Collections.reverseOrder());
+    // System.out.println(list);
+    // ArrayList<String> strings = new ArrayList<>();
+    // strings.add("Welcome");
+    // strings.add("to");
+    // strings.add("DSA");
+    // strings.add("Ramzan");
+    // System.out.println(strings);
+    // Collections.sort(strings, Collections.reverseOrder());
+    // System.out.println(strings);
 }
--- src/Test.java
+++ src/Test.java
@@ -1,6 +1,5 @@
 import java.util.ArrayList;
-
-import ArrayLists.Basics;
+import java.util.Collections;
 
 public class Test {
     public static void main(String[] args) {
@@ -12,6 +11,15 @@ public class Test {
         list.add(22);
         list.add(10);
         System.out.println(list);
-        Basics.reverseArrayList(list);
+        Collections.sort(list, Collections.reverseOrder());
+        System.out.println(list);
+        ArrayList<String> strings = new ArrayList<>();
+        strings.add("Welcome");
+        strings.add("to");
+        strings.add("DSA");
+        strings.add("Ramzan");
+        System.out.println(strings);
+        Collections.sort(strings, Collections.reverseOrder());
+        System.out.println(strings);
     }
 }

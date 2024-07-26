03e6155ce3d274ed97f43c016df3961a961f542d
Mohd Ramzan Shareef <107868540+itisRamzan@users.noreply.github.com>
2024-07-26 19:56:33 +0530

    Refactor Test.java and add ReviseEverything.java

---
+++ src/ReviseEverything.java
@@ -0,0 +1,5 @@
+public class ReviseEverything {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
--- src/Test.java
+++ src/Test.java
@@ -1,8 +1,6 @@
-import Strings.Basics;
 
 public class Test {
     public static void main(String[] args) {
-        String str = "abc";
         int[] arr = { 2, 4, 5 };
         Arrays.Subsets.printAllSubSequencesSum(arr, 0, 0);
     }

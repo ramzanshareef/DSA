6793f9ade5799929e122466b24e90a5cdc1763f8
Mohd Ramzan Shareef <107868540+itisRamzan@users.noreply.github.com>
2024-07-27 12:52:32 +0530

    chore: Add CoPilot.txt, Points.txt, and ReviseEverything.java

--- src/ReviseEverything.java
+++ src/ReviseEverything.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
+
 public class ReviseEverything {
+    static Scanner input = new Scanner(System.in);
+
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        int[] arr = { 900, 940, 950, 1100, 1500, 1800 };
+        int[] dep = { 910, 1200, 1120, 1130, 1900, 2000 };
+        int freqArr[] = new int[dep.length - arr.length];
+        freqArr[0] = Math.min(arr[0], dep[0]);
+        freqArr[freqArr.length] = Math.max(arr[arr.length - 1], dep[dep.length - 1]);
+
     }
 }
--- src/Solution.java
+++ src/Solution.java
@@ -1,24 +1,2 @@
 class Solution {
-    public static int[] productExceptSelf(int[] nums) {
-        int[] prefMul = new int[nums.length];
-        int[] suffMul = new int[nums.length];
-        int[] result = new int[nums.length];
-        prefMul[0] = 1;
-        suffMul[nums.length - 1] = 1;
-        for (int i = 1; i < nums.length; i++) {
-            prefMul[i] = nums[i - 1] * prefMul[i - 1];
-        }
-        for (int i = nums.length - 2; i >= 0; i--) {
-            suffMul[i] = nums[i + 1] * suffMul[i + 1];
-        }
-        for (int i = 0; i < result.length; i++) {
-            result[i] = prefMul[i] * suffMul[i];
-        }
-        return result;
-    }
-
-    public static void main(String[] args) {
-        int nums[] = { -1, 1, 0, -3, 3 };
-        productExceptSelf(nums);
-    }
 }

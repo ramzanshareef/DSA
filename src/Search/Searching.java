78f4315b4f5b04fea315e121265051dacc1843ba
Mohd Ramzan Shareef <107868540+itisRamzan@users.noreply.github.com>
2024-08-04 15:42:51 +0530

    Refactor binary search implementation and add methods for finding square root and minimum element in a rotated sorted array

--- src/Search/Searching.java
+++ src/Search/Searching.java
@@ -20,7 +20,7 @@ public class Searching {
         if (l > h)
             return -1;
         int mid = l + (h - l) / 2;
-        if (arr[mid] == target && arr[mid-1]!=target) {
+        if (arr[mid] == target && arr[mid - 1] != target) {
             return mid;
         } else if (arr[mid] > target) {
             return binarySearch(arr, target, l, mid - 1);
@@ -29,5 +29,37 @@ public class Searching {
         } else {
             return -1;
         }
+    }
+
+    public static int squareRoot(int x) {
+        int i = 0, j = x;
+        int ans = -1;
+        while (i <= j) {
+            int mid = i + (j - i) / 2;
+            int value = mid * mid;
+            if (value == x)
+                return mid;
+            else if (value < x) {
+                i = mid + 1;
+                ans = mid;
+            } else {
+                j = mid - 1;
+            }
+        }
+        return ans;
+    }
+
+    public static int minInRotatedSortedArray(int arr[]) {
+        int n = arr.length, low = 0, high = n - 1, ans = -1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (arr[mid] <= arr[n - 1]) {
+                ans = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return ans;
     }
 }
--- src/Test.java
+++ src/Test.java
@@ -2,7 +2,7 @@ import Search.Searching;
 
 public class Test {
     public static void main(String[] args) {
-        int arr[] = { 2, 4, 5, 7, 15, 20, 24, 45, 50, 77 };
-        System.out.println(Searching.binarySearch(arr, 24, 0, arr.length - 1));
+        int arr[] = { 3, 4, 5, 6, 7, 1, 2 };
+        System.out.println(Searching.minInRotatedSortedArray(arr));
     }
 }

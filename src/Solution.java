3db986906ce508a16912cf2536d8976b80352088
Mohd Ramzan Shareef <107868540+ramzanshareef@users.noreply.github.com>
2024-08-15 08:52:38 +0530

    feat: Add countSeniors method to Solution class

--- src/Solution.java
+++ src/Solution.java
@@ -1,29 +1,18 @@
-import java.util.Arrays;
-
 class Solution {
-    public int smallestDistancePair(int[] nums, int k) {
-        Arrays.sort(nums);
-        int low = 0;
-        int high = nums[nums.length - 1] - nums[0];
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-            int l = 0;
-            int r = 0;
-            int count = 0;
-            while (r < nums.length) {
-                if (nums[r] - nums[l] > mid) {
-                    l++;
-                } else {
-                    count += r - l;
-                    r++;
-                }
-            }
-            if (count >= k) {
-                high = mid - 1;
-            } else {
-                low = mid + 1;
+    public static int countSeniors(String[] citizens) {
+        int count = 0;
+        for (String citizen : citizens) {
+            int tensPlace = (citizen.charAt(11) - '0') * 10;
+            int onesPlace = (citizen.charAt(12) - '0');
+            if (tensPlace + onesPlace > 60) {
+                count++;
             }
         }
-        return low;
+        return count;
+    }
+
+    public static void main(String[] args) {
+        String citizens[] = { "7868190130M7522", "5303914400F9211", "9273338290F4010" };
+        System.out.println(countSeniors(citizens));
     }
 }

1b80330056285bb1fd13fa81c988835c1df3492f
Mohd Ramzan Shareef <107868540+ramzanshareef@users.noreply.github.com>
2024-08-16 09:10:44 +0530

    feat: Optimize maxDistance calculation in Solution class

--- src/Solution.java
+++ src/Solution.java
@@ -1,22 +1,17 @@
-class Solution {
-    public boolean lemonadeChange(int[] bills) {
-        int numberOfFives = 0, numberOfTens = 0;
-        for (int i : bills) {
-            if (i == 5) { // 5$
-                numberOfFives++;
-            } else if (i == 10) { // 10$
-                numberOfFives--;
-                numberOfTens++;
-            } else if (numberOfTens > 0 && numberOfFives >= 1) { // 20$
-                numberOfTens--;
-                numberOfFives--;
-            } else {
-                numberOfFives -= 3;
-            }
-            if (numberOfFives < 0) {
-                return false;
-            }
+import java.util.List;
+
+public class Solution {
+    public int maxDistance(List<List<Integer>> arrays) {
+        int res = Integer.MIN_VALUE;
+        int currMax = arrays.get(0).get(arrays.get(0).size() - 1);
+        int currMin = arrays.get(0).get(0);
+
+        for (int i = 1; i < arrays.size(); i++) {
+            res = Math.max(res, Math.abs(arrays.get(i).get(0) - currMax));
+            res = Math.max(res, Math.abs(arrays.get(i).get(arrays.get(i).size() - 1) - currMin));
+            currMax = Math.max(currMax, arrays.get(i).get(arrays.get(i).size() - 1));
+            currMin = Math.min(currMin, arrays.get(i).get(0));
         }
-        return true;
+        return res;
     }
 }

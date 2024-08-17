6ba0dc64bbcddbae0ec24fd6e021825daa512657
Mohd Ramzan Shareef <107868540+ramzanshareef@users.noreply.github.com>
2024-08-17 20:16:04 +0530

    refactor: Optimize setZeroes method in Solution class

--- src/Solution.java
+++ src/Solution.java
@@ -1,27 +1,47 @@
-import java.util.HashSet;
-import java.util.Set;
-
 class Solution {
-    public int longestConsecutive(int[] nums) {
-        if (nums.length == 0) {
-            return 0;
-        }
-        int longest = 1;
-        Set<Integer> set = new HashSet<>();
-        for (int i = 0; i < nums.length; i++) {
-            set.add(nums[i]);
-        }
-        for (int it : set) {
-            if (!set.contains(it - 1)) {
-                int count = 1;
-                int x = it;
-                while (set.contains(x + 1)) {
-                    x++;
-                    count++;
+    public static void setZeroes(int[][] matrix) {
+        int col0 = 1;
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (matrix[i][j] == 0) {
+                    matrix[i][0] = 0;
+                    if (j != 0) {
+                        matrix[0][j] = 0;
+                    } else {
+                        col0 = 0;
+                    }
                 }
-                longest = Math.max(longest, count);
             }
         }
-        return longest;
+        for (int i = 1; i < matrix.length; i++) {
+            for (int j = 1; j < matrix[i].length; j++) {
+                if (matrix[i][j] != 0) {
+                    if (matrix[0][j] == 0 || matrix[i][0] == 0) {
+                        matrix[i][j] = 0;
+                    }
+                }
+            }
+        }
+        if (matrix[0][0] == 0) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                matrix[0][j] = 0;
+            }
+        }
+        if (col0 == 0) {
+            for (int i = 0; i < matrix.length; i++) {
+                matrix[i][0] = 0;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[][] matrix = {
+                { 0, 1, 2, 0 },
+                { 3, 4, 5, 2 },
+                { 1, 3, 1, 5 }
+        };
+        Arrays.Basics.print2DArray(matrix);
+        setZeroes(matrix);
+        Arrays.Basics.print2DArray(matrix);
     }
 }

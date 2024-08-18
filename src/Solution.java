c9ce4a7d8848dbd869aa1515973bd5e1cfbd8cd1
Mohd Ramzan Shareef <107868540+ramzanshareef@users.noreply.github.com>
2024-08-18 11:29:19 +0530

    Optimize largestPalindrome method in Solution class

--- src/Solution.java
+++ src/Solution.java
@@ -1,16 +1,23 @@
+import java.util.Stack;
+
 class Solution {
-    public long maxEnergyBoost(int[] energyDrinkA, int[] energyDrinkB) {
-        int n = energyDrinkA.length;
-        long[] dpA = new long[n];
-        long[] dpB = new long[n];
-
-        dpA[0] = energyDrinkA[0];
-        dpB[0] = energyDrinkB[0];
-
-        for (int i = 1; i < n; i++) {
-            dpA[i] = Math.max(dpA[i - 1] + energyDrinkA[i], dpB[i - 1]);
-            dpB[i] = Math.max(dpB[i-1]+energyDrinkB[i], dpA[i-1]);
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<Character>();
+        for (char c : s.toCharArray()) {
+            if (c == '(' || c == '[' || c == '{') {
+                stack.push(c);
+            } else {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+                char top = stack.peek();
+                if ((c == ')' && top == '(') || (c == ']' && top == '[') || (c == '}' && top == '{')) {
+                    stack.pop();
+                } else {
+                    return false;
+                }
+            }
         }
-        return Math.max(dpA[n-1], dpB[n-1]);
+        return stack.isEmpty();
     }
 }

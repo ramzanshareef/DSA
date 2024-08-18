e7cacd88fe8fcaa4c7b98795e0dda761020bb27b
Mohd Ramzan Shareef <107868540+ramzanshareef@users.noreply.github.com>
2024-08-18 09:23:22 +0530

    Optimize maxEnergyBoost method in Solution class

--- src/Solution.java
+++ src/Solution.java
@@ -1,47 +1,16 @@
 class Solution {
-    public static void setZeroes(int[][] matrix) {
-        int col0 = 1;
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[i].length; j++) {
-                if (matrix[i][j] == 0) {
-                    matrix[i][0] = 0;
-                    if (j != 0) {
-                        matrix[0][j] = 0;
-                    } else {
-                        col0 = 0;
-                    }
-                }
-            }
-        }
-        for (int i = 1; i < matrix.length; i++) {
-            for (int j = 1; j < matrix[i].length; j++) {
-                if (matrix[i][j] != 0) {
-                    if (matrix[0][j] == 0 || matrix[i][0] == 0) {
-                        matrix[i][j] = 0;
-                    }
-                }
-            }
-        }
-        if (matrix[0][0] == 0) {
-            for (int j = 0; j < matrix[0].length; j++) {
-                matrix[0][j] = 0;
-            }
-        }
-        if (col0 == 0) {
-            for (int i = 0; i < matrix.length; i++) {
-                matrix[i][0] = 0;
-            }
-        }
-    }
+    public long maxEnergyBoost(int[] energyDrinkA, int[] energyDrinkB) {
+        int n = energyDrinkA.length;
+        long[] dpA = new long[n];
+        long[] dpB = new long[n];
 
-    public static void main(String[] args) {
-        int[][] matrix = {
-                { 0, 1, 2, 0 },
-                { 3, 4, 5, 2 },
-                { 1, 3, 1, 5 }
-        };
-        Arrays.Basics.print2DArray(matrix);
-        setZeroes(matrix);
-        Arrays.Basics.print2DArray(matrix);
+        dpA[0] = energyDrinkA[0];
+        dpB[0] = energyDrinkB[0];
+
+        for (int i = 1; i < n; i++) {
+            dpA[i] = Math.max(dpA[i - 1] + energyDrinkA[i], dpB[i - 1]);
+            dpB[i] = Math.max(dpB[i-1]+energyDrinkB[i], dpA[i-1]);
+        }
+        return Math.max(dpA[n-1], dpB[n-1]);
     }
 }

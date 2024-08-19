b90b2c506893386eb7728c996054f67b749652f8
Mohd Ramzan Shareef <107868540+ramzanshareef@users.noreply.github.com>
2024-08-19 07:07:36 +0530

    refactor: Optimize majorityElement method in Solution class

--- src/Solution.java
+++ src/Solution.java
@@ -1,23 +1,25 @@
-import java.util.Stack;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 class Solution {
-    public boolean isValid(String s) {
-        Stack<Character> stack = new Stack<Character>();
-        for (char c : s.toCharArray()) {
-            if (c == '(' || c == '[' || c == '{') {
-                stack.push(c);
-            } else {
-                if (stack.isEmpty()) {
-                    return false;
-                }
-                char top = stack.peek();
-                if ((c == ')' && top == '(') || (c == ']' && top == '[') || (c == '}' && top == '{')) {
-                    stack.pop();
-                } else {
-                    return false;
-                }
+    public static List<Integer> majorityElement(int[] nums) {
+        List<Integer> res = new ArrayList<>();
+        HashMap<Integer, Integer> cache = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            cache.put(nums[i], cache.getOrDefault(nums[i], 0) + 1);
+        }
+        for (Map.Entry<Integer, Integer> entry : cache.entrySet()) {
+            if (entry.getValue() > nums.length / 3) {
+                res.add(entry.getKey());
             }
         }
-        return stack.isEmpty();
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = { 1,2  };
+        System.out.println(majorityElement(nums));
     }
 }

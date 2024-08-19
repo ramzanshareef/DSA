d6110508bd86808567a1ce8184c1cf2e05f23bf6
Mohd Ramzan Shareef <107868540+ramzanshareef@users.noreply.github.com>
2024-08-19 07:13:52 +0530

    Optimize minSteps method in Solution class

--- src/Solution.java
+++ src/Solution.java
@@ -1,25 +1,17 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 class Solution {
-    public static List<Integer> majorityElement(int[] nums) {
-        List<Integer> res = new ArrayList<>();
-        HashMap<Integer, Integer> cache = new HashMap<>();
-        for (int i = 0; i < nums.length; i++) {
-            cache.put(nums[i], cache.getOrDefault(nums[i], 0) + 1);
-        }
-        for (Map.Entry<Integer, Integer> entry : cache.entrySet()) {
-            if (entry.getValue() > nums.length / 3) {
-                res.add(entry.getKey());
+    public int minSteps(int n) {
+        int res = 0;
+        for (int i = 2; i * i <= n;) {
+            if (n % i == 0) {
+                res += i;
+                n = n / i;
+            } else {
+                i++;
             }
         }
+        if (n != 1) {
+            res = res + n;
+        }
         return res;
-    }
-
-    public static void main(String[] args) {
-        int[] nums = { 1,2  };
-        System.out.println(majorityElement(nums));
     }
 }

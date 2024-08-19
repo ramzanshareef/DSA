647013c4de8df19c05e47042356a1a4577445a64
Mohd Ramzan Shareef <107868540+ramzanshareef@users.noreply.github.com>
2024-08-19 21:48:31 +0530

    refactor: Optimize majorityElement method in Solution class

--- src/Solution.java
+++ src/Solution.java
@@ -1,17 +1,42 @@
+import java.util.*;
+
 class Solution {
-    public int minSteps(int n) {
-        int res = 0;
-        for (int i = 2; i * i <= n;) {
-            if (n % i == 0) {
-                res += i;
-                n = n / i;
+    public List<Integer> majorityElement(int[] nums) {
+        int n = nums.length;
+        int count1 = 0, count = 0;
+        int firstEle = Integer.MIN_VALUE;
+        int secEle = Integer.MIN_VALUE;
+        for (int i = 0; i < n; i++) {
+            if (count1 == 0 && secEle != nums[i]) {
+                count1 = 1;
+                firstEle = nums[i];
+            } else if (count == 0 && firstEle != nums[i]) {
+                count = 1;
+                secEle = nums[i];
+            } else if (nums[i] == firstEle) {
+                count1++;
+            } else if (nums[i] == secEle) {
+                count++;
             } else {
-                i++;
+                count1--;
+                count--;
             }
         }
-        if (n != 1) {
-            res = res + n;
+        List<Integer> res = new ArrayList<>();
+        count1 = 0;
+        count = 0;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] == firstEle)
+                count1++;
+            if (nums[i] == secEle)
+                count++;
         }
+
+        int minLength = (int) (n / 3) + 1;
+        if (count1 >= minLength)
+            res.add(firstEle);
+        if (count >= minLength)
+            res.add(secEle);
         return res;
     }
 }

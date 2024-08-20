5e04f560d37e6c0349f198c4fe1c95a78ab45e4f
Mohd Ramzan Shareef <107868540+ramzanshareef@users.noreply.github.com>
2024-08-20 21:23:51 +0530

    refactor: Optimize fourSum method in Solution class

--- src/Solution.java
+++ src/Solution.java
@@ -1,32 +1,40 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 class Solution {
-    public List<List<Integer>> threeSum(int[] nums) {
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        int n = nums.length;
         List<List<Integer>> res = new ArrayList<>();
         Arrays.sort(nums);
-        int n = nums.length;
         for (int i = 0; i < n; i++) {
-            if (i != 0 && nums[i] == nums[i - 1])
+            if (i > 0 && nums[i] == nums[i - 1])
                 continue;
-            int j = i + 1;
-            int k = n - 1;
-            while (j < k) {
-                int sum = nums[i] + nums[j] + nums[k];
-                if (sum < 0) {
-                    j++;
-                } else if (sum > 0) {
-                    k--;
-                } else {
-                    List<Integer> temp = Arrays.asList(nums[i], nums[j], nums[k]);
-                    res.add(temp);
-                    j++;
-                    k--;
-                    while (j < k && nums[j] == nums[j - 1])
-                        j++;
-                    while (j < k && nums[k] == nums[k + 1])
-                        k--;
+            for (int j = i + 1; j < n; j++) {
+                if (j > i + 1 && nums[j] == nums[j - 1])
+                    continue;
+                int k = j + 1;
+                int l = n - 1;
+                while (k < l) {
+                    long sum = nums[i];
+                    sum += nums[j];
+                    sum += nums[k];
+                    sum += nums[l];
+                    if (sum == target) {
+                        List<Integer> temp = new ArrayList<>();
+                        temp.add(nums[i]);
+                        temp.add(nums[j]);
+                        temp.add(nums[k]);
+                        temp.add(nums[l]);
+                        res.add(temp);
+                        k++;
+                        l--;
+                        while (k < l && nums[k] == nums[k - 1])
+                            k++;
+                        while (k < l && nums[l] == nums[l + 1])
+                            l--;
+                    } else if (sum < target)
+                        k++;
+                    else
+                        l--;
                 }
             }
         }

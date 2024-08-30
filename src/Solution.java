ddb11fbecb7b03b59cf606f2e18666b72ad0d752
Mohd Ramzan Shareef <107868540+ramzanshareef@users.noreply.github.com>
2024-08-30 23:12:49 +0530

    refactor: Optimize sliding window algorithm for counting subarrays with sum less than k in binary array

--- src/Solution.java
+++ src/Solution.java
@@ -1,28 +1,32 @@
+import java.util.Arrays;
+
 class Solution {
-    public int minEatingSpeed(int[] piles, int h) {
-        int max = piles[0];
-        for (int i = 0; i < piles.length; i++) {
-            max = Math.max(max, piles[i]);
-        }
-        int low = 1;
-        int high = max;
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-            int val = totalHoursWithSpeekK(piles, mid);
-            if (val <= h) {
-                high = mid - 1;
+    public int countPartitions(int[] nums, int maxSum) {
+        int partitions = 1;
+        long subSum = 0;
+        for (int num : nums) {
+            if (subSum + num <= maxSum) {
+                subSum += num;
             } else {
+                partitions++;
+                subSum = num;
+            }
+        }
+        return partitions;
+    }
+
+    public int splitArray(int[] nums, int k) {
+        int low = Arrays.stream(nums).max().getAsInt();
+        int high = Arrays.stream(nums).sum();
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            int partitions = countPartitions(nums, mid);
+            if (partitions > k) {
                 low = mid + 1;
+            } else {
+                high = mid - 1;
             }
         }
         return low;
-    }
-
-    private int totalHoursWithSpeekK(int piles[], int k) {
-        int res = 0;
-        for (int i = 0; i < piles.length; i++) {
-            res += Math.ceil((double) piles[i] / (double) k);
-        }
-        return res;
     }
 }

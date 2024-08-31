3d9e8106ae109e18a044fbe70461c93babbe1e16
Mohd Ramzan Shareef <107868540+ramzanshareef@users.noreply.github.com>
2024-08-31 22:55:05 +0530

    refactor: Optimize linked list implementation and add basic operations

--- src/Solution.java
+++ src/Solution.java
@@ -1,28 +1,25 @@
 public class Solution {
-    public static String compressString(String s) {
-        StringBuilder res = new StringBuilder();
-        int currCount = 1;
-        char currChar = s.charAt(0);
-        for (int i = 1; i < s.length(); i++) {
-            char prevChar = s.charAt(i - 1);
-            currChar = s.charAt(i);
-            if (prevChar == currChar) {
-                currCount++;
-                if (currCount == 10) {
-                    res.append(9).append(prevChar);
-                    currCount = 1;
-                }
-            } else {
-                res.append(currCount).append(prevChar);
-                currCount = 1;
-            }
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
         }
-        res.append(currCount).append(currChar);
-        return res.toString();
     }
 
-    public static void main(String[] args) {
-        String s = "aaaaaaaaaaaaaabb";
-        System.out.println(compressString(s));
+    public ListNode middleNode(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null) {
+            if (fast.next!=null && fast.next.next != null) {
+                fast = fast.next.next;
+            } else {
+                fast = fast.next;
+            }
+            slow = slow.next;
+        }
+        return slow;
     }
 }

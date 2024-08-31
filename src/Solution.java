77c3f4361f0caff6f588596ebb3c017535b953dd
Mohd Ramzan Shareef <107868540+ramzanshareef@users.noreply.github.com>
2024-08-31 12:03:35 +0530

    refactor: Optimize sliding window algorithm for counting subarrays with sum less than k in binary array

--- src/Solution.java
+++ src/Solution.java
@@ -1,32 +1,8 @@
 import java.util.Arrays;
 
 class Solution {
-    public int countPartitions(int[] nums, int maxSum) {
-        int partitions = 1;
-        long subSum = 0;
-        for (int num : nums) {
-            if (subSum + num <= maxSum) {
-                subSum += num;
-            } else {
-                partitions++;
-                subSum = num;
-            }
-        }
-        return partitions;
-    }
-
-    public int splitArray(int[] nums, int k) {
-        int low = Arrays.stream(nums).max().getAsInt();
-        int high = Arrays.stream(nums).sum();
-        while (low <= high) {
-            int mid = (low + high) / 2;
-            int partitions = countPartitions(nums, mid);
-            if (partitions > k) {
-                low = mid + 1;
-            } else {
-                high = mid - 1;
-            }
-        }
-        return low;
+    public static void main(String[] args) {
+        int arr[] = {10, 20, 0, -5, 6, 180};
+        System.out.println(Arrays.stream(arr).max().getAsInt());
     }
 }

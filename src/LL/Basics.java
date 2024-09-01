cecf7cd7411e01a324ddea56da4afbf2c3b4b0db
Mohd Ramzan Shareef <107868540+ramzanshareef@users.noreply.github.com>
2024-09-01 14:45:09 +0530

    refactor: Optimize linked list implementation and add basic operations

--- src/LL/Basics.java
+++ src/LL/Basics.java
@@ -8,6 +8,9 @@ public class Basics {
         int val;
         ListNode next;
 
+        ListNode() {
+        }
+
         ListNode(int x) {
             val = x;
             next = null;
@@ -50,6 +53,70 @@ public class Basics {
             }
         }
         return false;
+    }
+
+    public ListNode detectCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        if (fast == null || fast.next == null) {
+            return null;
+        }
+        while (fast != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                break;
+            }
+        }
+        ListNode temp = head;
+        while (temp != slow) {
+            temp = temp.next;
+            slow = slow.next;
+        }
+        return slow;
+    }
+
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode t1 = list1;
+        ListNode t2 = list2;
+        ListNode head = new ListNode(100);
+        ListNode t = head;
+        while (t1 != null && t2 != null) {
+            if (t1.val < t2.val) {
+                t.next = t1;
+                t = t1;
+                t1 = t1.next;
+            } else {
+                t.next = t2;
+                t = t2;
+                t2 = t2.next;
+            }
+        }
+        if (t1 == null) {
+            t.next = t2;
+        } else {
+            t.next = t1;
+        }
+        return head.next;
+    }
+
+    public void evenOddLL(ListNode head) {
+        ListNode headOdd = new ListNode(-1);
+        ListNode headEven = new ListNode(-1);
+        ListNode tempOdd = headOdd;
+        ListNode tempEven = headEven;
+        ListNode temp = head;
+        while (temp != null) {
+            ListNode a = new ListNode(temp.val);
+            if (temp.val % 2 == 0) {
+                tempEven.next = a;
+                tempEven = a;
+            } else {
+                tempOdd.next = a;
+                tempOdd = a;
+            }
+            temp = temp.next;
+        }
     }
 
 }
--- src/Solution.java
+++ src/Solution.java
@@ -1,26 +1,42 @@
-public class Solution {
-    public class ListNode {
-        int val;
-        ListNode next;
+import java.util.*;
 
-        ListNode(int x) {
-            val = x;
-            next = null;
+public class Solution {
+    public int[] resultsArray(int[][] queries, int k) {
+        List<Integer> results = new ArrayList<>();
+        LinkedList<Integer> distances = new LinkedList<>();
+        HashMap<Integer, Integer> distanceMap = new HashMap<>();
+
+        for (int[] query : queries) {
+            int x = query[0];
+            int y = query[1];
+            int dist = Math.abs(x) + Math.abs(y);
+            if (distanceMap.containsKey(dist)) {
+                distanceMap.put(dist, distanceMap.get(dist) + 1);
+            } else {
+                distanceMap.put(dist, 1);
+            }
+            insertSorted(distances, dist);
+            if (distances.size() > k) {
+                int removedDist = distances.removeLast();
+                distanceMap.put(removedDist, distanceMap.get(removedDist) - 1);
+                if (distanceMap.get(removedDist) == 0) {
+                    distanceMap.remove(removedDist);
+                }
+            }
+            if (distances.size() < k) {
+                results.add(-1);
+            } else {
+                results.add(distances.get(k - 1));
+            }
         }
+        return results.stream().mapToInt(i -> i).toArray();
     }
 
-    public ListNode deleteMiddle(ListNode head) {
-        ListNode middleNode = head;
-        ListNode fast = head;
-        if (fast.next == null) {
-            head = null;
-            return null;
+    private void insertSorted(LinkedList<Integer> list, int dist) {
+        int index = 0;
+        while (index < list.size() && list.get(index) < dist) {
+            index++;
         }
-        while (fast.next.next.next != null && fast.next.next != null) {
-            fast = fast.next.next;
-            middleNode = middleNode.next;
-        }
-        middleNode.next = middleNode.next.next;
-        return head;
+        list.add(index, dist);
     }
 }

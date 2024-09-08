9b8c667f1c171aec49f703458ff428961bacfdd1
Mohd Ramzan Shareef <107868540+ramzanshareef@users.noreply.github.com>
2024-09-08 09:26:23 +0530

    Refactor gitignore and Test.java

--- src/Test.java
+++ src/Test.java
@@ -1,6 +1,9 @@
 public class Test {
     public static void main(String[] args) {
-        int[] arr = { 1, 0, 1, 0, 1 };
-        System.out.println(SlidingWindow.Code.countSubArraysWithSumKInBinaryArray(arr, 2));
+        // int[] arr = { 1, 0, 1, 0, 1 };
+        // System.out.println(SlidingWindow.Code.countSubArraysWithSumKInBinaryArray(arr,
+        // 2));
+        static int a = 50;
+        System.out.println(a);
     }
 }

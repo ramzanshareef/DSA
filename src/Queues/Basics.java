40e6a8d75aca57f3a8a2e033575af75029fec423
Mohd Ramzan Shareef <107868540+ramzanshareef@users.noreply.github.com>
2024-09-12 21:56:57 +0530

    Refactor Basics class to add queue reversal methods

--- src/Queues/Basics.java
+++ src/Queues/Basics.java
@@ -2,28 +2,54 @@ package Queues;
 
 import java.util.*;
 
-import Queues.Implementation.*;
-
 public class Basics {
     public static void main(String[] args) {
-        MeraQueueLL q = new MeraQueueLL();
+        Queue<Integer> q = new ArrayDeque<>();
         q.add(10);
         q.add(20);
         q.add(30);
         q.add(40);
         q.add(50);
-        System.out.println(q.peek());
+        printQueue(q);
+        reverseQueueK(q, 3);
+        System.out.println();
+        printQueue(q);
 
     }
 
     static void printQueue(Queue<Integer> q) {
         Queue<Integer> tq = new ArrayDeque<>();
         while (q.size() != 0) {
-            System.out.println(q.peek() + " ");
+            System.out.print(q.peek() + ", ");
             tq.add(q.poll());
         }
         while (tq.size() != 0) {
             q.add(tq.poll());
         }
     }
+
+    static void reverseQueue(Queue<Integer> q) {
+        Stack<Integer> st = new Stack<>();
+        while (!q.isEmpty()) {
+            st.push(q.poll());
+        }
+        while (!st.isEmpty()) {
+            q.add(st.pop());
+        }
+    }
+
+    static void reverseQueueK(Queue<Integer> q, int k) {
+        int length = q.size();
+        Stack<Integer> st = new Stack<>();
+        for (int i = 0; i < k; i++) {
+            st.push(q.poll());
+        }
+        while (!st.isEmpty()) {
+            q.add(st.pop());
+        }
+        for (int i = 0; i < length - k; i++) {
+            q.add(q.poll());
+        }
+    }
+
 }

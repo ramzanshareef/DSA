f202afe2d3070e8f6d17e6a8f8b625b0e0b5b116
Mohd Ramzan Shareef <107868540+ramzanshareef@users.noreply.github.com>
2024-10-06 08:58:00 +0530

    Refactor Solution.java to optimize remainingMethods() method Refactor Test.java to optimize character removal logic

--- src/Solution.java
+++ src/Solution.java
@@ -1,30 +1,50 @@
-public class Solution {
-    public boolean checkInclusion(String s1, String s2) {
-        if (s1.length() > s2.length()) {
-            return false;
+import java.util.*;
+
+class Solution {
+    public List<Integer> remainingMethods(int n, int k, int[][] invocations) {
+        List<List<Integer>> graph = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            graph.add(new ArrayList<>());
         }
-        int[] s1arr = new int[26];
-        for (int i = 0; i < s1.length(); i++) {
-            s1arr[s1.charAt(i) - 'a']++;
+        for (int[] invocation : invocations) {
+            graph.get(invocation[0]).add(invocation[1]);
         }
-        for (int i = 0; i <= s2.length() - s1.length(); i++) {
-            int[] s2arr = new int[26];
-            for (int j = 0; j < s1.length(); j++) {
-                s2arr[s2.charAt(i + j) - 'a']++;
+        Set<Integer> suspicious = new HashSet<>();
+        dfs(k, graph, suspicious);
+        boolean canRemove = true;
+        for (int i = 0; i < n; i++) {
+            if (!suspicious.contains(i)) {
+                for (int invokedMethod : graph.get(i)) {
+                    if (suspicious.contains(invokedMethod)) {
+                        canRemove = false;
+                        break;
+                    }
+                }
             }
-            if (matches(s1arr, s2arr)) {
-                return true;
+        }
+        List<Integer> result = new ArrayList<>();
+        if (canRemove) {
+            for (int i = 0; i < n; i++) {
+                if (!suspicious.contains(i)) {
+                    result.add(i);
+                }
+            }
+        } else {
+            for (int i = 0; i < n; i++) {
+                result.add(i);
             }
         }
-        return false;
+
+        return result;
     }
 
-    public boolean matches(int[] s1arr, int[] s2arr) {
-        for (int i = 0; i < 26; i++) {
-            if (s1arr[i] != s2arr[i]) {
-                return false;
-            }
+    private void dfs(int method, List<List<Integer>> graph, Set<Integer> suspicious) {
+        if (suspicious.contains(method)) {
+            return;
         }
-        return true;
+        suspicious.add(method);
+        for (int invokedMethod : graph.get(method)) {
+            dfs(invokedMethod, graph, suspicious);
+        }
     }
 }
--- src/Test.java
+++ src/Test.java
@@ -1,8 +1,15 @@
-import Stacks.Expressions;
+import java.util.*;
 
 public class Test {
     public static void main(String[] args) {
-        String str = "8-5+3*4/6";
-        System.out.println(Expressions.infixEval(str));
+        char charToRemove = 'a';
+        String str = "abdgf";
+        HashMap<Character, Integer> map = new HashMap<>();
+        for (char c : str.toCharArray()) {
+            map.merge(c, 1, Integer::sum);
+        }
+        System.out.println(map);
+        map.remove(charToRemove, 1);
+        System.out.println(map);
     }
 }
